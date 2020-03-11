ea5a9044d47dfafd0e9645b5c6ff961a61643c97
kouamano <amano.au1@gmail.com>
2020-03-11 15:27:19 +0900

    add

--- bio+informatics/make_pair1liner.pl
+++ bio+informatics/make_pair1liner.pl
@@ -9,20 +9,19 @@ $len = @arr;
 $arr[$len-1] =~ s/Database: .*//s;
 foreach(@arr){
 	$Q = $_;
-	@Q = split(/>/s,$Q);
+	@Q = split(/:>/s,$Q);
 	$H = shift(@Q);
 	"" =~ //;
 	$H =~ /([^\n]+)\n/;
 	$h = $1;
 	$h =~ s/\s//g;
 	foreach(@Q){
-		#print $_;
 		@hit = split(/\n/,$_);
 		$dbentry = shift(@hit);
 		$dbentry =~ s/\s/_/g;
-		$hit = join(/ ;; /,@hit);
-		print " $dbentry ;; ";
-		print " $h ;; ";
+		$hit = join(' ;; ',@hit);
+		print " =====$dbentry ;; ";
+		print " -----$h ;; ";
 		print "$hit ;; ";
 		print ";;;\n";
 	}
