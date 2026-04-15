a59ba766b3c654a6342ea69ee7d5b5c6208c2976
kouamano <amano.au1@gmail.com>
2026-04-15 16:26:59 +0900

    add

---
+++ text_op/remove-notascii.c
@@ -0,0 +1,45 @@
+/*remove-notascii.c*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define FILE_NAME_SIZE 1024
+
+/* input file */
+char file_name[FILE_NAME_SIZE];
+
+int main(int argc, char **argv){
+	/* vars */
+	FILE *IN;
+	int is_open = 0;
+	int C = 0;
+
+	/* open file */
+	if(argc == 1){
+		IN = stdin;
+	}else if(argc == 2){
+		sscanf(argv[1],"%s",file_name);
+		IN = fopen(file_name,"r");
+		if(IN == NULL){
+			perror(file_name);
+		}else{
+			is_open++;
+		}
+	}
+
+	/* func */
+	while((C = fgetc(IN)) != EOF){
+		if(C > 0x00 && C < 0x7f){
+			fprintf(stdout,"%c",C);
+		}else{
+			//putc(C,stdout);
+			//fprintf(stdout,"%c",C);
+			;
+		}
+	}
+	
+	/* close file */
+	if(is_open > 0){
+		fclose(IN);
+	}
+	exit(0);
+}
