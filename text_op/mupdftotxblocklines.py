aca754a329e8970e5a7910e65ed8974e4153f726
kouamano <amano.au1@gmail.com>
2026-09-11 11:42:46 +0900

    add

---
+++ text_op/mupdftotxblocklines.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import sys
+import pymupdf
+
+doc = pymupdf.open(sys.argv[1])
+
+for page in doc:
+	data = page.get_text("dict")
+	for block in data["blocks"]:
+		if "lines" not in block:
+			continue
+		for line in block["lines"]:
+			for span in line["spans"]:
+				print(
+					span["bbox"],
+					span["text"]
+				)
